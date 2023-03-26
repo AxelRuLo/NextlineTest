463eddec38392c279d9b743d1ff6ca6be306dca0
AxelRuLo <developeraxrulo@gmail.com>
2023-03-26 09:40:47 -0600

    develop: [REF] user contorller now catch exeptions and readme contains db instrucctions

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,29 +1,28 @@
-const user = require('../model/user_model')
-const controllerUser = {}
+const user = require("../model/user_model");
+const controllerUser = {};
 
 async function login(req, res) {
-    const username = req.body.username
-    const password = req.body.password
-    const session = req.session
+    const username = req.body.username;
+    const password = req.body.password;
+    const session = req.session;
 
-    if (username == '' || password == '') {
-        res.send('username and password must be provided').status(204)
+    if (username == "" || password == "") {
+        return res.send("username and password must be provided").status(204);
     }
 
-    const result = await user.login(username, password)
-    if (result[0]['iduser']) session.userid = result[0]['iduser']
+    const result = await user.login(username, password).catch((err) => err);
+    if (result[0]["iduser"]) session.userid = result[0]["iduser"];
 
-    res.send(result)
+    res.send(result);
 }
 
 async function logout(req, res) {
-    const session = req.session
-    session.destroy()
-    res.send('session destroyed').status(200)
+    const session = req.session;
+    session.destroy();
+    res.send("session destroyed").status(200);
 }
 
-controllerUser.login = login
-controllerUser.logout = logout
-
+controllerUser.login = login;
+controllerUser.logout = logout;
 
 module.exports = controllerUser;
